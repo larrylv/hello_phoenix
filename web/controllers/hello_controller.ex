997c19f838a4d90c8bde9188daece25b627bf3ca
Larry Lv <larrylv1990@gmail.com>
2015-09-05 21:12:36 +0800

    Channel rockes

--- web/controllers/hello_controller.ex
+++ web/controllers/hello_controller.ex
@@ -1,11 +1,15 @@
 defmodule HelloPhoenix.HelloController do
   use HelloPhoenix.Web, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
+  def index(conn, params) do
+    render conn, :index, message: params["message"]
   end
 
   def show(conn, %{"messenger" => messenger}) do
     render conn, "show.html", messenger: messenger
+  end
+
+  def test(conn, _params) do
+    render conn, :test
   end
 end
